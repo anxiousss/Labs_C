c280ab4b5a8b5dd486d093290d886b328802d6eb
anxiousss <saimonkheresh@gmail.com>
2024-02-28 12:54:47 +0300

    Update 14.c

--- 14/14.c
+++ 14/14.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SIZE 4
+const int SIZE = 4;  
 
-void printMatrixClockwise(int matrix[SIZE][SIZE]) {
+void printMatrixClockwise(int** matrix) {
     int rowStart = 0;
     int rowEnd = SIZE - 1;
     int colStart = 0;
@@ -11,44 +12,52 @@ void printMatrixClockwise(int matrix[SIZE][SIZE]) {
 
     while (rowStart <= rowEnd && colStart <= colEnd) {
         // Вывод элементов верхней строки
-        for (i = colStart; i <= colEnd; i++) {
-            printf("%d ", matrix[rowStart][i]);
+        for (i = colEnd; i >= colStart; --i) {
+            printf("%d ", *(*(matrix + rowStart) + i));
         }
-        rowStart++;
+        ++rowStart;
 
         // Вывод элементов последнего столбца
-        for (i = rowStart; i <= rowEnd; i++) {
-            printf("%d ", matrix[i][colEnd]);
+        for (i = rowStart; i <= rowEnd; ++i) {
+            printf("%d ", *(*(matrix + i) + colStart));
         }
-        colEnd--;
+        ++colStart;
 
         // Вывод элементов нижней строки в обратном порядке
         if (rowStart <= rowEnd) {
-            for (i = colEnd; i >= colStart; i--) {
-                printf("%d ", matrix[rowEnd][i]);
+            for (i = colStart; i <= colEnd; ++i) {
+                printf("%d ", *(*(matrix + rowEnd) + i));
             }
-            rowEnd--;
+            --rowEnd;
         }
 
         // Вывод элементов первого столбца в обратном порядке
         if (colStart <= colEnd) {
-            for (i = rowEnd; i >= rowStart; i--) {
-                printf("%d ", matrix[i][colStart]);
+            for (i = rowEnd; i >= rowStart; --i) {
+                printf("%d ", *(*(matrix + i) + colEnd));
             }
-            colStart++;
+            --colEnd;
         }
     }
 }
 
 int main() {
-    int matrix[SIZE][SIZE] = {
-        {4, 5, 6, 7},
-        {3, 14, 15, 8},
-        {2, 13, 16, 9},
-        {1, 12, 11, 10}
-    };
+    int** matrix = (int**)(malloc(sizeof(int*) * SIZE));
+
+    for (int i = 0; i < SIZE; ++i) {
+        *(matrix + i) = (int*)(malloc(sizeof(int) * SIZE));
+
+        for (int j = 0; j < SIZE; ++j) {
+            scanf("%d", (*(matrix + i)) + j);
+        }
+    }
 
     printMatrixClockwise(matrix);
 
+    for (int i = 0; i < SIZE; ++i) {
+        free(*(matrix + i));
+    }
+
+    free(matrix);   
     return 0;
 }
