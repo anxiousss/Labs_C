1ca013cdb95ff1458e3d67aa6bbb828151e7901e
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-05-18 23:29:30 +0300

    upd py file for generation

--- Practice/6/main.py
+++ Practice/6/main.py
@@ -3,14 +3,13 @@ import names
 
 
 with open('test.txt', 'w') as fin:
-    n_students = random.randint(1, 100)
+    n_students = 50
     for i in range(n_students):
         surname = names.get_last_name()
         initials = surname[:3]
         school_number = random.randint(100, 2000)
         medal = random.randint(0, 1)
-        scores = random.randint(50, 100)
+        scores = random.randint(2, 5)
         credit = random.randint(0, 1)
         fin.write(str(surname) + ' ' + str(initials) + ' ' + str(school_number) + ' ' +
                   str(medal) + ' ' + str(scores) + ' ' + str(credit) + '\n')
-
