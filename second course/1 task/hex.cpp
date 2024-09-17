24f22b8d61379ee180adbf14a243252e5cc9274d
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-09-17 23:41:16 +0300

    h file

---
+++ second course/1 task/hex.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#include "hex.h"
+
+void ReverseArray(int arr[], int size)
+{
+    for (int i = 0; i < size / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+}
+
+int* bin(int number, int* len, int* digits) {
+    int i = 0;
+    while (number != 0) {
+        int residue = number % 2;
+        number /= 2;
+        digits[i] = residue;
+        ++i;
+    }
+
+    *len = i;
+    ReverseArray(digits, i);
+    return digits;
+}
+
+void hex(int* binary_number, int len, int* discharges) {
+    int k = 0;
+    for (int i = len - 1; i > -1; i -= 4) {
+        int degree = 0;
+        int discharge = 0;
+        for (int j = i; j != i - 4; --j) {
+            discharge += binary_number[j] * (pow(2, degree));
+            degree += 1;
+        }
+        discharges[k] = discharge;
+        ++k;
+    }
+
+    for (int i = k - 1; i > -1; --i) {
+        printf("%X", discharges[i]);
+    }
+}
---
+++ second course/1 task/hex.h
@@ -0,0 +1,7 @@
+
+
+void ReverseArray(int arr[], int size);
+
+int* bin(int number, int* len, int* digits);
+
+void hex(int* binary_number, int len, int* discharges);
