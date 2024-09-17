8e1413e5067f04ae1eb3aea85e1e13eda6c930f0
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-09-17 23:15:03 +0300

    s ,c a flags done

--- second course/1 task/1.c
+++ second course/1 task/1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+#include "hex.h"
 
 typedef enum kOpts {
     OPT_H,
@@ -11,37 +13,45 @@ typedef enum kOpts {
     OPT_F
 } kOpts;
 
-int GetOpts(int argc, char** argv, kOpts *option, int *number) {
+
+int flag_recognizing(char* str, const char* flag1, const char* flag2) {
+    return strcmp(str, flag1) == 0 && strcmp(str, flag2);
+}
+
+
+int GetOpts(int argc, char** argv, kOpts* option, int* number) {
     if (argc < 2) {
-        // Если нет аргументов, считаем это ошибкой
         return 1;
     }
 
     *number = atoi(argv[1]);
-    if (strcmp(argv[2], "-h") == 0) {
+    if (flag_recognizing(argv[2], "-h", "/h")) {
         *option = OPT_H;
         return 0;
-    } else if (strcmp(argv[2], "-p") == 0) {
+    }
+    else if (flag_recognizing(argv[2], "-p", "/p")) {
         *option = OPT_P;
         return 0;
-    } else if (strcmp(argv[2], "-s") == 0) {
+    }
+    else if (flag_recognizing(argv[2], "-s", "/s")) {
         *option = OPT_S;
         return 0;
-    } else if (strcmp(argv[2], "-c") == 0) {
+    }
+    else if (flag_recognizing(argv[2], "-c", "/c")) {
         *option = OPT_C;
         return 0;
-    } else if (strcmp(argv[2], "-a") == 0) {
+    }
+    else if (flag_recognizing(argv[2], "-a", "/a")) {
         *option = OPT_A;
         return 0;
-    } else if (strcmp(argv[2], "-f") == 0) {
+    }
+    else if (flag_recognizing(argv[2], "-f", "/f")) {
         *option = OPT_F;
         return 0;
     }
-    
+
     return 1;
 }
-
-
 
 
 void HandlerOptH(int number) {
@@ -52,7 +62,7 @@ void HandlerOptH(int number) {
             printf("%d ", x);
         }
     }
-    
+
     if (!flag) {
         printf("No numbers");
     }
@@ -60,32 +70,44 @@ void HandlerOptH(int number) {
 
 void HandlerOptP(int number) {
     int flag = 0;
-    for (int x = 2; x < number / 2 + 1; ++x) {
+    for (int x = 2; x < (int)sqrt(number) + 1; ++x) {
         if (number % x == 0) {
-            flag = 1;
             printf("composite number");
-            break;
+            return;
         }
     }
-    if (!flag) {
-        printf("simple number");
-    }
+    printf("simple number");
 }
 void HandlerOptS(int number) {
-    
+    int len = 0;
+    int digits[33];
+    int discharges[9];
+    hex(bin(number, &len, digits), len, discharges);
 }
-void HandlerOptC(int number);
-void HandlerOptA(int number);
-void HandlerOptF(int number);
+
+void HandlerOptC(int number) {
+    for (int x = 1; x < number + 1; ++x) {
+        for (int d = 1; d < 11; ++d) {
+            printf("%f ", pow(x, d));
+        }
+        printf("\n");
+    }
+}
+
+void HandlerOptA(int number) {
+    printf("%d", ((1 + number) / 2) * (number + 1));
+}
 
 
-int main (int argc, char** argv) {
-    kOpts opt = 0;
+int main(int argc, char** argv) {
+    kOpts opt = OPT_H;
     int procceed_number = 0;
     void (*handlers[6])(int) = {
         HandlerOptH,
         HandlerOptP,
-        HandlerOptS
+        HandlerOptS,
+        HandlerOptC,
+        HandlerOptA
     };
 
     if (GetOpts(argc, argv, &opt, &procceed_number)) {
