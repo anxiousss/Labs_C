be0e366733ceb72b70bc40b45ecab82cc945a463
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-09-23 23:02:53 +0300

    first task complete

--- second course/1 task/1.c
+++ second course/1 task/1.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <math.h>
 #include "hex.h"
 
@@ -13,12 +11,6 @@ typedef enum kOpts {
     OPT_F
 } kOpts;
 
-
-int flag_recognizing(char* str, const char* flag1, const char* flag2) {
-    return strcmp(str, flag1) == 0 && strcmp(str, flag2);
-}
-
-
 int GetOpts(int argc, char** argv, kOpts* option, int* number) {
     if (argc != 3) {
         return 1;
@@ -29,27 +21,26 @@ int GetOpts(int argc, char** argv, kOpts* option, int* number) {
         if (procceding_option[0] == '/' || procceding_option[0] == '-') {
             switch (procceding_option[1])
             {
-            case 'h':
-                *option = OPT_H;
-                break;
-            case 'p':
-                *option = OPT_P;
-                break;
-            case 's':
-                *option = OPT_S;
-                break;
-            case 'e':
-                *option = OPT_E;
-                break;
-            case 'a':
-                *option = OPT_A;
-                break;
-            case 'f':
-                *option = OPT_F;
-                break;
-            default:
-                return 1;
-                break;
+                case 'h':
+                    *option = OPT_H;
+                    break;
+                case 'p':
+                    *option = OPT_P;
+                    break;
+                case 's':
+                    *option = OPT_S;
+                    break;
+                case 'e':
+                    *option = OPT_E;
+                    break;
+                case 'a':
+                    *option = OPT_A;
+                    break;
+                case 'f':
+                    *option = OPT_F;
+                    break;
+                default:
+                    return 1;
             }
         }
         else {
@@ -65,7 +56,7 @@ int GetOpts(int argc, char** argv, kOpts* option, int* number) {
         }
     }
     return 0;
-}   
+}
 
 void HandlerOptH(int number) {
     int flag = 0;
@@ -82,7 +73,13 @@ void HandlerOptH(int number) {
 }
 
 void HandlerOptP(int number) {
-    int flag = 0;
+
+    if (number == 1) {
+        printf("non-simple non-composite");
+        return;
+    }
+
+
     for (int x = 2; x < (int)sqrt(number) + 1; ++x) {
         if (number % x == 0) {
             printf("composite number");
@@ -95,7 +92,7 @@ void HandlerOptS(int number) {
     int len = 0;
     int digits[33];
     int discharges[9];
-    hex(bin(number, &len, digits), len, discharges);
+    hex(bin(number, &len, digits), &len, discharges);
 }
 
 void HandlerOptE(int number) {
@@ -111,15 +108,30 @@ void HandlerOptA(int number) {
     printf("%d", ((1 + number) / 2) * (number + 1));
 }
 
+void HandlerOptF(int number) {
+    long long factor = 1;
+    for (int x = 2; x < number + 1; ++x) {
+        factor *= x;
+    }
+    if (factor < 0) {
+        printf("Overflow detected.\n");
+        return;
+    }
+
+    printf("%lld", factor);
+}
+
 
 int main(int argc, char** argv) {
     kOpts opt = OPT_H;
     int procceed_number = 0;
     void (*handlers[6])(int) = {
-        HandlerOptH,
-        HandlerOptP,
-        HandlerOptE,
-        HandlerOptA
+            HandlerOptH,
+            HandlerOptP,
+            HandlerOptS,
+            HandlerOptE,
+            HandlerOptA,
+            HandlerOptF
     };
 
     if (GetOpts(argc, argv, &opt, &procceed_number)) {
