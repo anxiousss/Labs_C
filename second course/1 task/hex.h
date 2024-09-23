70f2f2389a084ee9045cbf58b0e18b7c9ca4d0f9
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-09-23 23:03:28 +0300

    Update hex.h

--- second course/1 task/hex.h
+++ second course/1 task/hex.h
@@ -1,7 +1,5 @@
-
-
 void ReverseArray(int arr[], int size);
 
 int* bin(int number, int* len, int* digits);
 
-void hex(int* binary_number, int len, int* discharges);
+void hex(int* binary_number, const int* len, int* discharges);
