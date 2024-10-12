1d75443d21efc8eb338bdee20c6ddcbf70c2ff6e
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-10-13 00:27:12 +0300

    big upd task 7 part 3

--- second course/7 task/solution.c
+++ second course/7 task/solution.c
@@ -43,71 +43,133 @@ int base_conversion(int number, char* result, int based) {
     return 0;
 }
 
+void free_buffer(WordsBuffer* buffer) {
+    if (buffer == NULL)
+        return;
 
-int split(FILE* file, char*** buffer, int* word_index, int* char_index, int* buffer_size, int* word_size) {
-    char c;
-    while ((c = fgetc(file)) != EOF) {
-        if (c != ' ' && c != '\n' && c != '\t') {
-            if (*word_index >= *buffer_size)  {
-                *buffer_size *= 2;
-                char** tmp1 = (char** )realloc(*buffer, sizeof(char*) * *buffer_size);
-                if (tmp1 == NULL) {
-                    free(tmp1);
-                    return Memory_leak;
-                }
-                *buffer = tmp1;
-                for (int i = *word_index; i < *buffer_size; ++i) {
-                    (*buffer)[i] = (char* ) malloc(*word_size * sizeof(char));
-                }
-            }
-            if (*char_index >= *word_size) {
-                *word_size *= 2;
-                char* tmp2 = (char* )realloc((*buffer)[*word_index], sizeof(char) * (*word_size));
-                if (tmp2 == NULL) {
-                    free(tmp2);
-                    return Memory_leak;
-                }
-                (*buffer)[*word_index] = tmp2;
-                for (int i = *char_index; i < *word_size; ++i) {
-                    (*buffer)[*word_index][i] = ' ';
-                }
-            }
-            (*buffer)[*word_index][*char_index] = c;
-            (*char_index)++;
-        } else {
-            if (*char_index > 0) {
-                (*buffer)[*word_index][*char_index] = '\0';
-                (*word_index)++;
-                *char_index = 0;
-            }
-        }
+    for (int i = 0; i < buffer->size; i++) {
+        free(buffer->words[i]);
     }
-    if (*char_index > 0) {
-        (*buffer)[*word_index][*char_index] = '\0';
-        (*word_index)++;
-    }
-    return 0;
+
+    free(buffer->words);
+    free(buffer->word_sizes);
+    free(buffer->word_capacities);
+    free(buffer);
 }
 
-int buffer_alloc(char*** buffer, int buffer_size, int word_size) {
-    *buffer = (char**)malloc(buffer_size * sizeof(char*));
-    if (*buffer == NULL) {
-        return Memory_leak;
+
+WordsBuffer* create_buffer() {
+    WordsBuffer* buffer = (WordsBuffer*)malloc(sizeof(WordsBuffer));
+    if (buffer == NULL)
+        return NULL;
+
+    buffer->capacity = 4;
+    buffer->size = 0;
+
+    buffer->words = (char**)malloc(sizeof(char*) * buffer->capacity);
+    buffer->word_sizes = (int*)malloc(sizeof(int) * buffer->capacity);
+    buffer->word_capacities = (int*)malloc(sizeof(int) * buffer->capacity);
+
+    if (buffer->words == NULL || buffer->word_sizes == NULL || buffer->word_capacities == NULL) {
+        free(buffer);
+        return NULL;
     }
-    for (int i = 0; i < buffer_size; ++i) {
-        (*buffer)[i] = (char*)malloc(word_size * sizeof(char));
-        if ((*buffer)[i] == NULL) {
-            for (int j = 0; j < i; ++j) {
-                free((*buffer)[j]);
-            }
-            free(*buffer);
+
+    return buffer;
+}
+
+int insert_symbol(WordsBuffer* buffer, char c) {
+    if (buffer == NULL || buffer->size == 0)
+        return Memory_leak;
+
+    int last_i = buffer->size - 1;
+
+    char* last_word = buffer->words[last_i];
+    int len = buffer->word_sizes[last_i];
+    int capacity = buffer->word_capacities[last_i];
+
+    if (len == capacity) {
+        buffer->word_capacities[last_i] *= 2;
+        char* new_word = (char*)realloc(last_word, sizeof(char) * buffer->word_capacities[last_i]);
+
+        if (new_word == NULL) {
             return Memory_leak;
         }
+
+        buffer->words[last_i] = new_word;
     }
+
+    buffer->words[last_i][buffer->word_sizes[last_i]] = c;
+    buffer->word_sizes[last_i]++;
+
     return 0;
 }
 
+int insert_word(WordsBuffer* buffer) {
+    if (buffer == NULL)
+        return Memory_leak;
 
+    if (buffer->size == buffer->capacity) {
+        buffer->capacity *= 2;
+        char** new_words = (char**)realloc(buffer->words, sizeof(char*) * buffer->capacity);
+        int* new_word_sizes = (int*)realloc(buffer->word_sizes, sizeof(int) * buffer->capacity);
+        int* new_word_capacities = (int*)realloc(buffer->word_capacities, sizeof(int) * buffer->capacity);
+
+        if (new_words == NULL || new_word_sizes == NULL || new_word_capacities == NULL) {
+            return Memory_leak;
+        }
+
+        buffer->words = new_words;
+        buffer->word_sizes = new_word_sizes;
+        buffer->word_capacities = new_word_capacities;
+    }
+
+    buffer->word_capacities[buffer->size] = 4;
+    buffer->word_sizes[buffer->size] = 0;
+
+    buffer->words[buffer->size] = (char*)malloc(sizeof(char) * buffer->word_capacities[buffer->size]);
+    if (buffer->words[buffer->size] == NULL)
+        return Memory_leak;
+
+    buffer->size++;
+    return 0;
+}
+
+int split_by_words(WordsBuffer* buffer, FILE* file) {
+    if (buffer == NULL || file == NULL)
+        return Memory_leak;
+
+    char c;
+    int err;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (buffer->size > 0) {
+                err = insert_symbol(buffer, '\0');
+                if (err == Memory_leak)
+                    return err;
+
+            }
+            err = insert_word(buffer);
+            if (err == Memory_leak)
+                return err;
+        } else {
+            if (buffer->size == 0) {
+                err = insert_word(buffer);
+                if (err == Memory_leak)
+                    return err;
+            }
+            err = insert_symbol(buffer, c);
+            if (err == Memory_leak)
+                return err;
+        }
+    }
+
+    if (buffer->size > 0) {
+        insert_symbol(buffer, '\0');
+    }
+
+    return 0;
+}
 int HandlerOptR(char** paths) {
     if (strcmp(paths[0], paths[1]) == 0) {
         return Equal_paths;
@@ -124,79 +186,44 @@ int HandlerOptR(char** paths) {
         return Memory_leak;
     }
 
-    char** buffer1 = NULL;
-
-    int buffer_size1 = 10, word_size1 = 5;
-    int alloc_res1 = buffer_alloc(&buffer1, buffer_size1, word_size1);
-
-    if (alloc_res1) {
-        fclose(fin1);
-        fclose(fin2);
-        fclose(out);
-        return alloc_res1;
+    WordsBuffer* words_buffer1 = create_buffer();
+    WordsBuffer* words_buffer2 = create_buffer();
+    if (words_buffer1 == NULL || words_buffer2 == NULL) {
+        return Memory_leak;
     }
+    int res1 = split_by_words(words_buffer1, fin1);
+    int res2 = split_by_words(words_buffer2, fin2);
 
-    char** buffer2 = NULL;
-    int  buffer_size2 = 10, word_size2 = 5;
-    int alloc_res2 = buffer_alloc(&buffer2, buffer_size2, word_size2);
-
-    if (alloc_res2) {
-        fclose(fin1);
-        fclose(fin2);
-        fclose(out);
-        return alloc_res2;
-    }
-
-    int word_index1 = 0, word_index2 = 0, char_index1 = 0, char_index2 = 0;
-    int res1 = split(fin1, &buffer1, &word_index1, &char_index1, &buffer_size1, &word_size1);
-    int res2 = split(fin2, &buffer2, &word_index2, &char_index2, &buffer_size2, &word_size2);
-
-    if (res1 || res2) {
-        fclose(fin1);
-        fclose(fin2);
-        fclose(out);
-        // Освобождаем уже выделенную память для buffer1 и buffer2
-        for (int i = 0; i < buffer_size1; ++i) {
-            free(buffer1[i]);
-        }
-        free(buffer1);
-        for (int i = 0; i < buffer_size2; ++i) {
-            free(buffer2[i]);
-        }
-        free(buffer2);
+    if (res1 != 0 || res2 != 0) {
+        free_buffer(words_buffer1);
+        free_buffer(words_buffer2);
         return Memory_leak;
     }
 
+    int n_words1 = words_buffer1->size;
+    int n_words2 = words_buffer2->size;
 
-    int max_words = word_index1 > word_index2 ? word_index1 : word_index2;
+    int max_words = n_words1 > n_words2 ? n_words1 : n_words2;
 
     for (int i = 0; i < max_words; ++i) {
 
-        if (i < word_index1 && i < word_index2) {
+        if (i < n_words1 && i < n_words2) {
             if (i % 2 == 0) {
-                fprintf(out, "%s ", buffer1[i]);
+                fprintf(out, "%s ", words_buffer1->words[i]);
             } else {
-                fprintf(out, "%s ", buffer2[i]);
+                fprintf(out, "%s ", words_buffer2->words[i]);
             }
         }
-        else if (i < word_index1) {
-            fprintf(out, "%s ", buffer1[i]);
+        else if (i < n_words1) {
+            fprintf(out, "%s ",words_buffer1->words[i]);
         }
-        else if (i < word_index2) {
-            fprintf(out, "%s ", buffer2[i]);
+        else if (i < n_words2) {
+            fprintf(out, "%s ", words_buffer2->words[i]);
         }
     }
 
-    // Освобождаем память
-    for (int i = 0; i < buffer_size1; ++i) {
-        free(buffer1[i]);
-    }
-    free(buffer1);
-    for (int i = 0; i < buffer_size2; ++i) {
-        free(buffer2[i]);
-    }
-    free(buffer2);
-
+    free_buffer(words_buffer1);
+    free_buffer(words_buffer2);
     fclose(fin1);
     fclose(fin2);
     fclose(out);
@@ -217,73 +244,60 @@ int HandlerOptA(char** paths) {
         return Memory_leak;
     }
 
-    char** buffer1 = NULL;
-    int buffer_size1 = 10, word_size1 = 5;
-    int alloc_res1 = buffer_alloc(&buffer1, buffer_size1, word_size1);
 
-    if (alloc_res1) {
-        fclose(fin1);
-        fclose(out);
-        return alloc_res1;
-    }
-
-    int word_index1 = 0, char_index1 = 0;
-    int res1 = split(fin1, &buffer1, &word_index1, &char_index1, &buffer_size1, &word_size1);
-
-    if (res1) {
-        fclose(fin1);
-        fclose(out);
-        for (int i = 0; i < buffer_size1; ++i) {
-            free(buffer1[i]);
-        }
-        free(buffer1);
+    WordsBuffer* words_buffer1 = create_buffer();
+    
+    if (words_buffer1 == NULL) {
         return Memory_leak;
     }
+    int res1 = split_by_words(words_buffer1, fin1);
 
+    if (res1 != 0) {
+        free_buffer(words_buffer1);
+        return Memory_leak;
+    }
+    
     // logic
 
-    for (int i = 0; i < buffer_size1; ++i) {
-        if (i % 10 == 0) {
-            for (int j = 0; buffer1[i][j] != '\0'; ++j) {
-                if (buffer1[i][j] >= 'A' && buffer1[i][j] <= 'Z')
-                    buffer1[i][j] = 'a' + (buffer1[i][j] - 'A');
+    for (int i = 0; i < words_buffer1->size; ++i) {
+        if (i % 10 == 9) {
+            for (int j = 0; words_buffer1->words[i][j] != '\0'; ++j) {
+                if (words_buffer1->words[i][j] >= 'A' && words_buffer1->words[i][j] <= 'Z')
+                    words_buffer1->words[i][j] = 'a' + (words_buffer1->words[i][j] - 'A');
             }
-            for (int j = 0; j < buffer1[i][j] != '\0'; ++j) {
+            for (int j = 0; j < words_buffer1->words[i][j] != '\0'; ++j) {
                 char result[65];
-                int err = base_conversion(buffer1[i][j], result, 4);
+                int err = base_conversion(words_buffer1->words[i][j], result, 4);
                 if (err) {
+                    free_buffer(words_buffer1);
                     return err;
                 }
                 fprintf(out, "%s", result);
                 putc(' ', out);
             }
-        } else if (i % 2 == 0) {
-            for (int j = 0; buffer1[i][j] != '\0'; ++j) {
-                if (buffer1[i][j] >= 'A' && buffer1[i][j] <= 'Z')
-                    buffer1[i][j] = 'a' + (buffer1[i][j] - 'A');
+        } else if (i % 2 == 1) {
+            for (int j = 0; words_buffer1->words[i][j] != '\0'; ++j) {
+                if (words_buffer1->words[i][j] >= 'A' && words_buffer1->words[i][j] <= 'Z')
+                    words_buffer1->words[i][j] = 'a' + (words_buffer1->words[i][j] - 'A');
             }
-            fprintf(out, "%s ", buffer1[i]);
-        } else if (i % 5 == 0) {
-            for (int j = 0; j < buffer1[i][j] != '\0'; ++j) {
+            fprintf(out, "%s ", words_buffer1->words[i]);
+        } else if (i % 5 == 4) {
+            for (int j = 0; j < words_buffer1->words[i][j] != '\0'; ++j) {
                 char result[65];
-                int err = base_conversion(buffer1[i][j], result, 8);
+                int err = base_conversion(words_buffer1->words[i][j], result, 8);
                 if (err) {
+                    free_buffer(words_buffer1);
                     return err;
                 }
                 fprintf(out, "%s", result);
                 putc(' ', out);
             }
         }  else {
-            fprintf(out, "%s ", buffer1[i]);
+            fprintf(out, "%s ", words_buffer1->words[i]);
         }
     }
 
-    for (int i = 0; i < buffer_size1; ++i) {
-        free(buffer1[i]);
-    }
-    free(buffer1);
-
-
+    free_buffer(words_buffer1);
     fclose(fin1);
     fclose(out);
 
