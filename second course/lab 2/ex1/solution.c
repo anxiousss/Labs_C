75598ab17d7f0bb08aaa3ca6c9680b7aa95c218c
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-10-21 21:26:32 +0300

    reupload solution files

--- second course/lab 2/ex1/solution.c
+++ second course/lab 2/ex1/solution.c
@@ -1,45 +1,164 @@
-
 #include "solution.h"
 
-
-
-double geometric_mean(int n, ...) {
-    if (n <= 0) {
-        return 0.0;
+int len(const char* str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        ++i;
     }
+    return i;
+}
 
-    va_list args;
-    va_start(args, n);
+void reverseString(char *str) {
+    int length = len(str);
+    char *start = str;
+    char *end = str + length - 1;
+    char temp;
 
-    double product = 1.0;
-    for (int i = 0; i < n; ++i) {
-        double num = va_arg(args, double);
-        product *= num;
+    while (start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        ++start;
+        --end;
     }
-
-    va_end(args);
-
-    return pow(product, 1.0 / n);
 }
 
 
-double recurse_exp(double base, int exponent) {
-    if (exponent == 0) {
-        return 1.0;
-    }
-
-    if (exponent > 0) {
-        if (exponent % 2 == 0) {
-            double temp = recurse_exp(base, exponent / 2);
-            return temp * temp;
+int my_atoi(const char* str, unsigned int* number) {
+    *number = 0;
+    for (int i = 0; str[i]; ++i) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            *number *= 10;
+            *number += (str[i] - '0');
         }
         else {
-            return base * recurse_exp(base, exponent - 1);
+            return Invalid_input;
+        }
+    }
+    return 0;
+}
+
+void string_copy(char *dst, const char *src) {
+    char *dstaddr = dst;
+    while((*dst++ = *src++));
+}
+
+char* my_strcat(char *dest, const char *src) {
+    char *ptr = dest + len(dest);
+
+    while (*src != '\0') {
+        *ptr++ = *src++;
+    }
+
+    *ptr = '\0';
+
+    return dest;
+}
+
+
+int is_num(int x) {
+    return x >= '0' && x <= '9';
+}
+
+int is_letter(int x) {
+    return (x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z');
+}
+
+int is_alnum(int x) {
+    return is_num(x) || is_letter(x);
+}
+
+
+
+int my_toupper(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return c - 'a' + 'A';
+    }
+    return c;
+}
+
+int HandlerOptL(char** argv, int* size) {
+    *size = len(argv[2]);
+    return 0;
+}
+
+int HandlerOptR(char** argv, char** str) {
+    int string_size = len(argv[2]);
+    *str = (char* ) malloc(sizeof(char) * (string_size + 1));
+    string_copy(*str, argv[2]);
+    if (!*str) return Memory_leak;
+    reverseString(*str);
+    return 0;
+}
+
+int HandlerOptU(char** argv, char** str) {
+    int string_size = len(argv[2]);
+    *str = (char* ) malloc(sizeof(char) * (string_size + 1));
+    if (!*str) return Memory_leak;
+    string_copy(*str, argv[2]);
+    for (int i = 0; (*str)[i] != '\0'; ++i) {
+        if (i % 2 == 1) {
+            (*str)[i] = (char) my_toupper((*str)[i]);
+        }
+    }
+    (*str)[string_size] = '\0';
+    return 0;
+}
+
+int HandlerOptN(char** argv, char** str) {
+    int string_size = len(argv[2]);
+    *str = (char* )(malloc(sizeof(char) * (string_size + 1)));
+    if (!*str) return Memory_leak;
+    string_copy(*str, argv[2]);
+    int index = 0;
+    for (int i = 0; argv[2][i] != '\0'; ++i) {
+        if (is_num(argv[2][i])) {
+            (*str)[index] = argv[2][i];
+            ++index;    
         }
     }
 
-    if (exponent < 0) {
-        return 1.0 / recurse_exp(base, -exponent);
+    for (int i = 0; argv[2][i] != '\0'; ++i) {
+        if (is_letter(argv[2][i])) {
+            (*str)[index] = argv[2][i];
+            ++index;
+        }
+    }
+
+    for (int i = 0; argv[2][i] != '\0'; ++i) {
+        if (!is_alnum(argv[2][i])) {
+            (*str)[index] = argv[2][i];
+            ++index;
+        }
+    }
+    (*str)[index] = '\0';
+    return 0;
+}
+
+int HandlerOptC(int argc, char** argv, char** str, unsigned int* seed) {
+    int err = my_atoi(argv[3], seed);
+    if (err)
+        return err;
+    int all_string_size  = len(argv[2]);
+    srand(*seed);
+    for (int i = 4; i < argc; ++i) {
+        all_string_size += len(argv[i]);
+    }
+    *str = (char* ) malloc(sizeof(char) * (all_string_size + 1));
+    if (!*str) return Memory_leak;
+    (*str)[0] = '\0';
+    for (int j = 3; j < argc - 1; ++j) {
+        argv[j] = argv[j + 1];
+    }
+    int size = argc - 3, random_index;
+    for (int i = 0; i < argc - 3; ++i) {
+        random_index = rand() % size + 2;
+        *str = my_strcat(*str, argv[random_index]);
+        for (int j = random_index; j < argc - 1; ++j) {
+            argv[j] = argv[j + 1];
+        }
+        --size;
     }
     return 0;
 }
--- second course/lab 2/ex1/solution.h
+++ second course/lab 2/ex1/solution.h
@@ -1,17 +1,48 @@
 
+
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
 #include <stdio.h>
-#include <math.h>
-#include <stdarg.h>
+#include <stdlib.h>
+
+typedef enum kOpts {
+    OPT_L = 1,
+    OPT_R,
+    OPT_U,
+    OPT_N,
+    OPT_C
+} kOpts;
 
 typedef enum Errors {
-    Invalid_input = 2
+    Invalid_input = 2,
+    Memory_leak
 } Errors;
 
-double geometric_mean(int n, ...);
+int len(const char* str);
 
-double recurse_exp(double x, int n);
+void reverseString(char *str);
+
+void string_copy(char *dst, const char *src);
+
+int my_atoi(const char* str, unsigned int* number);
+
+int my_toupper(int c);
+
+int is_num(int x);
+
+int is_alnum(int x);
+
+int is_letter(int x);
+
+int HandlerOptL(char** argv, int* size);
+
+int HandlerOptR(char** argv, char** str );
+
+int HandlerOptU(char** argv, char** str);
+
+int HandlerOptN(char** argv, char** str);
+
+int HandlerOptC(int argc, char** argv, char** str, unsigned int* seed);
 
 #endif //SOLUTION_H
