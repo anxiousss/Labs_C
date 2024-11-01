b485e2f1a1d10cc389570237fb7bda96b129e218
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-11-01 23:56:30 +0300

    string.h 3.4 task

---
+++ second course/FundAlg/lab 3/ex4/string.h
@@ -0,0 +1,35 @@
+
+#ifndef EX3_4_STRING_H
+#define EX3_4_STRING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct String {
+  char* mas;
+  int size;
+  int capacity;
+} String;
+
+typedef enum Errors {
+    Memory_leak = 3
+} Errors;
+
+int len(const char* str);
+
+int init_string(String* string, char* src);
+
+void delete_string(String* string);
+
+int cmp_string(String* string1, String* string2);
+
+int eq_string(String* string1, String* string2);
+
+int copy_str(String* dst, String* src);
+
+int copy_newstr(String* dst, String* src);
+
+int concatenation(String* A, String* B);
+
+#endif //EX3_4_STRING_H
